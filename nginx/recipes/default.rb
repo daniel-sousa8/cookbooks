53118f9dd461317b5afd9dbfa146cc49484f697f
daniel-sousa8 <daniel.sousa@rivendel.com.br>
2016-08-24 15:14:12 -0300

    Update default.rb

--- nginx/recipes/default.rb
+++ nginx/recipes/default.rb
@@ -7,6 +7,8 @@
 # All rights reserved - Do Not Redistribute
 #
 
+node[:deploy].each do |application, deploy|
+
 package "nginx" do
   action :install
 end
@@ -16,8 +18,11 @@ template '/etc/nginx/sites-available/default' do
   owner "www-data" 
   group "root"
   mode 00644
+  variables({  :app_name => "#{application}"})
 end
 
 service "nginx" do
   action [ :enable ]
+end
+
 end
