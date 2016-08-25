20d93a0a2e1a4ac1615a288dad3bc20056fb5041
daniel-sousa8 <daniel.sousa@rivendel.com.br>
2016-08-25 14:21:17 -0300

    Create cron.rb

---
+++ php/recipes/cron.rb
@@ -0,0 +1,10 @@
+node[:deploy].each do |application, deploy|
+
+template "/var/spool/cron/crontabs/deploy" do
+  source "crontab_api.erb"
+  mode "0660"
+  group "www-data"
+  owner "deploy"
+  variables({ :app_name => "#{application}"})
+end
+end
